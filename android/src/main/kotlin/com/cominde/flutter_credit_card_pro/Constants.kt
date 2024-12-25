38ec7c4622fff14c5caf43b9833918daf58c28f5
joeshwoa <joeshwoa.george@gmail.com>
2024-12-25 16:00:41 +0200

    ^1.0.0

---
+++ android/src/main/kotlin/com/cominde/flutter_credit_card_pro/Constants.kt
@@ -0,0 +1,7 @@
+object Constants {
+    const val GYROSCOPE_CHANNEL_NAME: String = "com.cominde.flutter_credit_card_pro/gyroscope"
+    const val METHOD_CHANNEL_NAME: String = "com.cominde.flutter_credit_card_pro"
+    const val INITIATE_EVENTS: String = "initiateEvents"
+    const val IS_GYRO_AVAILABLE: String = "isGyroscopeAvailable"
+    const val CANCEL_EVENTS: String = "cancelEvents"
+}
---
+++ ios/Classes/Constants.swift
@@ -0,0 +1,7 @@
+enum Constants {
+    static let gyroEventChannel = "com.cominde.flutter_credit_card_pro/gyroscope"
+    static let gyroMethodChannel = "com.cominde.flutter_credit_card_pro"
+    static let initiateEvents = "initiateEvents"
+    static let cancelEvents = "cancelEvents"
+    static let isGyroAvailable = "isGyroscopeAvailable"
+}
